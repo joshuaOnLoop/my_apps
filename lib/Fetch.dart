0dad2b2e4fd24ae794b340f4bada091d212cb81f
joshuaOnLoop <88298923+joshuaOnLoop@users.noreply.github.com>
2021-08-12 17:31:12 +0800

    Added fetch widget to explore with Future Object

--- lib/Fetch.dart
+++ lib/Fetch.dart
@@ -24,22 +24,33 @@ class Fetch extends StatefulWidget {
 }
 
 class _FetchState extends State<Fetch> {
-  Future<Album> futureAlbum;
-
-  Future<Album> fetchAlbum() async {
-    final response = await http
-        .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/2'));
-
-    if (response.statusCode == 200)
-      return Album.fromJson(jsonDecode(response.body));
-    else
-      throw Exception('Failed to load album');
-  }
+  Future<Album> _futureAlbum;
+  final TextEditingController _controller = TextEditingController();
+  var x = 5;
 
   @override
   void initState() {
     super.initState();
-    futureAlbum = fetchAlbum();
+    _controller.text = "https://jsonplaceholder.typicode.com/albums/1";
+    fetchAlbum();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void fetchAlbum() async {
+    final response = await http.get(Uri.parse(_controller.text));
+
+    if (response.statusCode == 200) {
+      this._futureAlbum = Future<Album>.value(Album.fromJson(jsonDecode(response.body)));
+      setState(() {
+      
+      });
+    } else
+      throw Exception('Failed to load album');
   }
 
   @override
@@ -49,15 +60,29 @@ class _FetchState extends State<Fetch> {
           title: Text("My Fetch Demo"),
           actions: [],
         ),
-        body: FutureBuilder<Album>(
-            future: this.futureAlbum,
-            builder: (context, snapshot) {
-              if (snapshot.hasData)
-                return Text(snapshot.data.title);
-              else if (snapshot.hasError) return Text('${snapshot.error}');
-
-              // By default, show a loading spinner.
-              return const CircularProgressIndicator();
-            }));
+        body: Column(
+          children: [
+            Padding(
+                padding: EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: _controller,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(), hintText: "Enter URL"),
+                )),
+            FutureBuilder<Album>(
+                future: this._futureAlbum,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData)
+                    return Text(snapshot.data.title);
+                  else if (snapshot.hasError) return Text('${snapshot.error}');
+                  // By default, show a loading spinner.
+                  return const CircularProgressIndicator();
+                }),
+            RaisedButton(
+              child : Text("Fetch"),
+              onPressed: fetchAlbum,
+            )
+          ],
+        ));
   }
 }
